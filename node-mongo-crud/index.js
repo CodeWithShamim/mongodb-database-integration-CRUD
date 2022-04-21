42dbaf2d7a797ad843f68a5c3117a33f5af826c2
codewithshamim <shamimislamonline@gmail.com>
2022-04-21 19:34:32 +0600

    delete data from database

--- node-mongo-crud/index.js
+++ node-mongo-crud/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -20,17 +20,29 @@ async function run() {
         await client.connect();
         const userCollection = client.db("usersDB").collection("users");
 
+        // get data 
         app.get('/users', async(req, res) => {
             const query = {};
             const cursor = userCollection.find(query);
             const users = await cursor.toArray();
             res.send(users);
         })
+
+        // post dat 
         app.post('/users', async(req, res) => {
             const newUser = req.body;
             // console.log(newUser);
             const result = await userCollection.insertOne(newUser);
             res.send(result.insertedId);
+        })
+
+        // delete data 
+        app.delete('/user/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+
         })
 
     } finally {
--- react-node-mongo/src/components/Home/Home.js
+++ react-node-mongo/src/components/Home/Home.js
@@ -9,11 +9,35 @@ const Home = () => {
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
+
+  const handleUserDelete = (id) => {
+    const proceed = window.confirm("Are you sure you want to delete!!");
+    console.log(proceed, id);
+    if (proceed) {
+      const url = `http://localhost:5000/user/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            const remaining = users.filter((user) => user._id !== id);
+            setUsers(remaining);
+            alert("Congrats, Successfully deleted");
+          }
+        });
+    }
+  };
   return (
     <div>
       <h1>Users: {users.length}</h1>
       {users.map((user) => (
-        <h3>{user.email}</h3>
+        <div key={user._id}>
+          <h3>
+            {user.email}{" "}
+            <button onClick={() => handleUserDelete(user._id)}>X</button>
+          </h3>
+        </div>
       ))}
     </div>
   );
