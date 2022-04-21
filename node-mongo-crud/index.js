b6a25c72c3f0d64591425b764216f666cbb5a0f0
codewithshamim <shamimislamonline@gmail.com>
2022-04-21 14:20:11 +0600

    send data to database using mongodb indertOne

--- node-mongo-crud/index.js
+++ node-mongo-crud/index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+// mongodb connected ===========
+const uri = "mongodb+srv://userdb:userdb100@cluster0.41efc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("usersDB").collection("users");
+        const users = { id: 1, name: 'shamim100', email: 'shamim@gmail.com' }
+        const result = await userCollection.insertOne(users);
+        console.log("This document successfully inserted with id is :", result.insertedId)
+
+    } finally {
+        // await client.close();
+
+    }
+}
+// call function 
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send({ id: 1, user: "shamim" });
